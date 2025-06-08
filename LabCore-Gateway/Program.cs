d5c75683b468f3c2c2fdcb2004511607f8c88034
Ziad KADDARI <ziadkaddari1@gmail.com>
2025-06-08 22:17:16 +0100

    first commit

---
+++ LabCore-Gateway/Program.cs
@@ -0,0 +1,52 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("Services/AuthService/ocelot.auth.json", optional: false)
+    .AddJsonFile("Services/ocelot.global.json", optional: false)
+    .Build();
+
+builder.Services.AddOcelot(config);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("AllowAll");
+
+await app.UseOcelot();
+
+await app.RunAsync();
